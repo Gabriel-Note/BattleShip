a04d07a545c3696462599da75310108ae264ecbd
gabriel note <gabriel.emil.note@gmail.com>
2026-03-22 15:15:07 +0100

    Created PlayerTurnDto
    holds row, col and playerNumber to determine where to shoot and on what board

---
+++ src/main/java/WebGames/GN/BattleShip/dto/PlayerTurnDto.java
@@ -0,0 +1,34 @@
+package WebGames.GN.BattleShip.dto;
+
+public class PlayerTurnDto {
+    int row;
+    int column;
+    int playerNumber;
+
+    public PlayerTurnDto() {
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+}
